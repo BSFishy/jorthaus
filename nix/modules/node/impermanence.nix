dd5ee401b5c2f1ffd9b1e4e099f69219993a73d3
Matt Provost <mattprovost6@gmail.com>
2026-08-22 14:09:38 +0000

    fix: persist ssh keys
    this was causing the whole /etc/ssh directory to be nuked when
    installing properly. that meant sshd could not start because
    /etc/ssh/sshd_config did not exist. instead we just persist the ssh keys
    because those are what we actually care about anyway
    
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- nix/modules/node/impermanence.nix
+++ nix/modules/node/impermanence.nix
@@ -44,11 +44,14 @@ in
 
       files = [
         "/etc/machine-id"
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
+        "/etc/ssh/ssh_host_rsa_key"
+        "/etc/ssh/ssh_host_rsa_key.pub"
       ]
       ++ cfg.files;
 
       directories = [
-        "/etc/ssh"
         "/var/lib/nixos"
         "/var/lib/systemd"
         "/var/log"
