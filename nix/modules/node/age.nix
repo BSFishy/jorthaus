888e9f72c41cc6c7f0f66b9cae00b380d4b96ea5
Matt Provost <mattprovost6@gmail.com>
2026-08-22 15:49:07 +0000

    fix: use persisted ssh keys for activation
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- nix/modules/node/age.nix
+++ nix/modules/node/age.nix
@@ -2,4 +2,11 @@
 
 {
   imports = [ inputs.agenix.nixosModules.default ];
+
+  # Use the persisted SSH host keys so agenix can decrypt secrets during
+  # activation before /etc/ssh is populated with the runtime key files.
+  age.identityPaths = [
+    "/persistent/etc/ssh/ssh_host_ed25519_key"
+    "/persistent/etc/ssh/ssh_host_rsa_key"
+  ];
 }
